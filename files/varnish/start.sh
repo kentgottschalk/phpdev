05d690f3816a58f5be42e6a4effa4fda11e71f8d
holt83 <simoh@vejlebib.dk>
2020-01-29 10:14:02 +0100

    Use exec command in start.sh script

--- files/varnish/start.sh
+++ files/varnish/start.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /var/lib/varnish/`hostname` && chown nobody /var/lib/varnish/`hostname`
 
-varnishd -j unix,user=varnish -F -S 'none' -s malloc,${VARNISH_MEMORY} -a :80 -T :6082 -f ${VARNISH_CONFIG_FILE}
+exec varnishd -j unix,user=varnish -F -S 'none' -s malloc,${VARNISH_MEMORY} -a :80 -T :6082 -f ${VARNISH_CONFIG_FILE}
 
 sleep 1
 varnishlog
