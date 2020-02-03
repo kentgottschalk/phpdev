79426307b2b7ed5b60a64ecc4f39bd42a0eec902
Simon Holt <simonholt83@gmail.com>
2020-02-04 00:18:55 +0100

    Use correct path to varnish config file

--- files/varnish/start.sh
+++ files/varnish/start.sh
@@ -3,7 +3,7 @@
 mkdir -p /var/lib/varnish/`hostname` && chown nobody /var/lib/varnish/`hostname`
 
 if [ -n "${VARNISH_CONFIG_FILE}" ]; then
-  exec varnishd -j unix,user=varnish -F -S 'none' -s malloc,${VARNISH_MEMORY} -a :80 -T :6082 -f ${VARNISH_CONFIG_FILE}
+  exec varnishd -j unix,user=varnish -F -S 'none' -s malloc,${VARNISH_MEMORY} -a :80 -T :6082 -f "/etc/varnish/${VARNISH_CONFIG_FILE}"
 else
   exec varnishd -j unix,user=varnish -F -S 'none' -s malloc,${VARNISH_MEMORY} -a :80 -T :6082 -b ${VARNISH_BACKEND_ADDRESS}:${VARNISH_BACKEND_PORT}
 fi
